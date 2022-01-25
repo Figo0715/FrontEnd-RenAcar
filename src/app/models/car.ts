5e60b744c4ec27c362ee8a509e7f4794954bca3d
Ömer Çetinkaya <omercetinkaya07@hotmail.com>
2022-01-25 23:28:55 +0300

    25.01.2022

---
+++ src/app/models/car.ts
@@ -0,0 +1,10 @@
+export interface Car{
+    id:number;
+    brandId:number;
+    colorId:number;
+    minFindexScore:number;
+    modelName:string;
+    modelYear:number;
+    dailyPrice:number;
+    description:string;
+}
